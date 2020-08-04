01bef580cfa4ec6ff92e3910bb38891343e27c3c
Efe <efe@houseofapps.com>
2020-08-04 15:28:16 +0300

    post clone is changed

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,7 +1,13 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 
-# Example: Clone a required repository
-git clone https://github.com/example/SomeProject
+# # Example: Clone a required repository
+# git clone https://github.com/example/SomeProject
 
-# Example: Install App Center CLI
-npm install -g appcenter-cli
+# # Example: Install App Center CLI
+# npm install -g appcenter-cli
+
+# Force the 1.5.3 version of cocoapods
+echo "uninstalling all cocoapods versions"
+sudo gem uninstall cocoapods --all
+echo "installing cocoapods version "
+sudo gem install cocoapods -v 1.9.4
